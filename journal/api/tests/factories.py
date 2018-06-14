1b842c7173513397156ec614ed798eafb036b773
Alexander Gor <ornot.work@gmail.com>
2018-06-14 15:17:37 +0300

    Revert "T3-138 Added history crud"
    This reverts commit f6ae6d42b9fc4aed6288a280a6e4451fe3a3707e.

--- journal/api/tests/factories.py
+++ journal/api/tests/factories.py
@@ -1,19 +1,13 @@
 import factory
 
-from journal.api.models import Journal, HistoryRecord
+from journal.api.models import Journal
 
 
 class JournalFactory(factory.DjangoModelFactory):
     class Meta:
         model = Journal
 
-    alias = 'clients'
     type = 'objects'
     history_record_key = 'id'
     history_record_actor = 'actor.id'
     save_diff = True
-
-
-class HistoryRecordFactory(factory.DjangoModelFactory):
-    class Meta:
-        model = HistoryRecord
